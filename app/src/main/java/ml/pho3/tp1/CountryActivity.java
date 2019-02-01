faf761409d265b58b7ba1438ea4c4720c43ce65e
Pho3 <pho3bechan@gmail.com>
2019-02-01 22:18:11 +0100

    Some layout fixes

--- app/src/main/java/ml/pho3/tp1/CountryActivity.java
+++ app/src/main/java/ml/pho3/tp1/CountryActivity.java
@@ -3,7 +3,6 @@ package ml.pho3.tp1;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ImageView;
