4f48eeb2f400fa5b084106ebfe6390f6b972248e
Pho3 <pho3bechan@gmail.com>
2019-02-27 23:22:35 +0100

    changes

--- app/src/main/java/ml/pho3/tp3/MainActivity.java
+++ app/src/main/java/ml/pho3/tp3/MainActivity.java
@@ -2,21 +2,76 @@ package ml.pho3.tp3;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import ml.pho3.tp3.adapter.WeatherAdapter;
+import ml.pho3.tp3.data.City;
+import ml.pho3.tp3.data.WeatherDbHelper;
 
 public class MainActivity extends Activity {
+
+    private WeatherDbHelper wd;
+    private WeatherAdapter wa;
+    private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        wd = new WeatherDbHelper(this);
+        Log.e("onCreate@WeatherDbHelpe", "Populate");
+        if(!wd.isCreated()) wd.populate();
 
+        Cursor c = wd.fetchAllCities();
+
+        final ListView listview = (ListView) findViewById(R.id.listView);
+
+        wa = new WeatherAdapter(this, c);
+
+        listview.setAdapter(wa);
+        listview.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        listview.setItemChecked(2, true);
+
+        registerForContextMenu(listview);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                Cursor item = (Cursor) parent.getItemAtPosition(position);
+                City city = wd.cursorToCity(item);
+                Intent i = new Intent(getApplicationContext(), CityActivity.class);
+
+                i.putExtra("edit", 1);
+                i.putExtra(City.TAG, city);
+
+                Log.e("city_name","> "+city.getName());
+
+                startActivityForResult(i, 1);
+
+            }
+        });
+
+        getActionBar().setDisplayShowCustomEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
+        getMenuInflater().inflate(R.menu.addbar, menu);
+        return true;
     }
 
 }
---
+++ app/src/main/java/ml/pho3/tp3/adapter/WeatherAdapter.java
@@ -0,0 +1,43 @@
+package ml.pho3.tp3.adapter;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import ml.pho3.tp3.R;
+import ml.pho3.tp3.data.WeatherDbHelper;
+
+public class WeatherAdapter extends CursorAdapter {
+    public WeatherAdapter(Context context, Cursor cursor) {
+        super(context, cursor, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.row, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView Name = (TextView) view.findViewById(R.id.cName);
+        TextView Country = (TextView) view.findViewById(R.id.cCountry);
+        TextView Temperature = (TextView) view.findViewById(R.id.temperature);
+        ImageView bookCover = (ImageView) view.findViewById(R.id.imageViewRow);
+
+        String name = cursor.getString(cursor.getColumnIndexOrThrow(WeatherDbHelper.COLUMN_CITY_NAME));
+        String count = cursor.getString(cursor.getColumnIndexOrThrow(WeatherDbHelper.COLUMN_COUNTRY));
+        String temp = cursor.getString(cursor.getColumnIndexOrThrow(WeatherDbHelper.COLUMN_TEMPERATURE));
+        bookCover.setImageResource(R.drawable.weather_sunny);
+
+        Name.setText(name);
+        Country.setText(count);
+        Temperature.setText(temp);
+    }
+
+}
